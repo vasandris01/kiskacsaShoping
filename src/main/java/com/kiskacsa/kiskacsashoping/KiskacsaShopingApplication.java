bb0400ece898c432d52a468963d0232169d2b714
vasandris01 <undris01@gmail.com>
2023-02-21 14:29:50 +0100

    Initial commit

---
+++ src/main/java/com/kiskacsa/kiskacsashoping/KiskacsaShopingApplication.java
@@ -0,0 +1,13 @@
+package com.kiskacsa.kiskacsashoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiskacsaShopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiskacsaShopingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kiskacsa/kiskacsashoping/Product.java
@@ -0,0 +1,2 @@
+package com.kiskacsa.kiskacsashoping;public class Produckt {
+}
